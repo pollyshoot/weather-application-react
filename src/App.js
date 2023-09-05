78afac5128e16e4951d8a2ba190ec10cd435e831
Polina Radahuz <polina.radahuz@gmail.com>
2023-09-05 15:10:03 +0200

    api works with the default city

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="weather-application mt-5">
+
+          <form className="row m-1">
+            <input type="search" placeholder="Type a city" className="col-6" />
+            <input type="submit" value="Search" className="col-2" />
+          </form>
+
+          <Weather defaultCity="Sydney" />
+
+          <div className="forecast-container" id="forecast"></div>
+        </div>
+
+        <footer className="source">
+          <a
+            href="https://github.com/pollyshoot/shecodesplus_project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+          , by Polina Radahuz
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+
+  let [weather, setWeather] = useState({ ready: false });
+  function getWeather(response) {
+    setWeather({
+      ready: true,
+      city: response.data.name,
+      date: "Monday 21.08.2020 13:44",
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].main,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  if (weather.ready) {
+    return (
+      <div>
+        <h1 id="current-city">{weather.city}</h1>
+        <ul>
+          <li id="description" className="description text-capitalize">
+            {weather.description}
+          </li>
+          <li>
+            <div id="time">{weather.date}</div>
+          </li>
+        </ul>
+
+        <div className="current-weather row">
+          <div className="current-temperature_container col-6">
+            <div className="d-flex">
+              <img src={weather.iconUrl} alt={weather.description} />
+              <div className="current-temperature">
+                <span className="temperature" id="temperature">
+                  {weather.temperature}
+                </span>
+                <span className="units">°C</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="current-weather_details col-6 d-flex align-items-center">
+            <ul>
+              <li id="humidity">
+                Humidity: <span>{weather.humidity}%</span>
+              </li>
+              <li id="wind">
+                Wind: <span>{weather.wind} m/s</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "0479fec9478c6c9031d035f5c5efc126";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getWeather);
+    return "Loading...";
+  }
+}
